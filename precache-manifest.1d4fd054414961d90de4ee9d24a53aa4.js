a45eb284b0c5b106be20b6ae3c9ef440cfa72523
alakaktyc <40630262+alakaktyc@users.noreply.github.com>
2019-07-18 18:57:24 +0300

    Updates

---
+++ precache-manifest.1d4fd054414961d90de4ee9d24a53aa4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c031033f7d28df4e9093612e432d316",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "eff132b8e80b4c87023f",
+    "url": "/Weather/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "5365ef4c5b013bd921af",
+    "url": "/Weather/static/css/main.6d493823.chunk.css"
+  },
+  {
+    "revision": "eff132b8e80b4c87023f",
+    "url": "/Weather/static/js/2.e597a5a4.chunk.js"
+  },
+  {
+    "revision": "5365ef4c5b013bd921af",
+    "url": "/Weather/static/js/main.fe99b096.chunk.js"
+  },
+  {
+    "revision": "e6172640d3267d8b322d",
+    "url": "/Weather/static/js/runtime~main.72be58e2.js"
+  },
+  {
+    "revision": "cc54c19b29890841267ee0750eb25a00",
+    "url": "/Weather/static/media/bg.cc54c19b.jpg"
+  }
+]);
